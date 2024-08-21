7eb7cda2c5ffe75e449bfb613d48cde4015c975e
Samanta1984 <samantaramos1984@hotmail.com>
2024-08-21 14:22:59 +0200

    portfolio

---
+++ scripts.js
@@ -0,0 +1,11 @@
+
+
+function showContactInfo() {
+    document.getElementById("contact-info").style.display = "block";
+}
+
+function closeContactInfo() {
+    document.getElementById("contact-info").style.display = "none";
+}
+
+
