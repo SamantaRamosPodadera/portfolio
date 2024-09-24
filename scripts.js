3f2c5c0057b535efb5ebe4873c461385fa71966e
Samanta Ramos Podadera <161749959+SamantaRamosPodadera@users.noreply.github.com>
2024-09-24 21:15:45 +0200

    indext

--- scripts.js
+++ scripts.js
@@ -8,23 +8,7 @@ function closeContactInfo() {
     document.getElementById("contact-info").style.display = "none";
 }
 
-// Mostrar el botón de "Volver al inicio" al hacer scroll
-window.onscroll = function() {
-    const scrollToTopBtn = document.getElementById("scrollToTop");
-    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-        scrollToTopBtn.style.display = "block"; // Mostrar el botón
-    } else {
-        scrollToTopBtn.style.display = "none"; // Ocultar el botón
-    }
-};
 
-// Desplazar hacia arriba suavemente al hacer clic
-document.getElementById("scrollToTop").onclick = function(event) {
-    event.preventDefault(); // Prevenir el comportamiento predeterminado del enlace
-    window.scrollTo({
-        top: 0,
-        behavior: "smooth" // Desplazamiento suave
-    });
-};
+
 
 
